958613c464dc08f0c045dd8e02d023101327f79e
Blake Douglass <blakecdouglass@gmail.com>
2023-08-13 20:12:44 -0700

    major improvements to GFY page

--- app/_components/FeaturedPosts.tsx
+++ app/_components/FeaturedPosts.tsx
@@ -6,6 +6,7 @@ import PostCard from "@/app/_components/PostCard";
 export default function FeaturedPosts({ posts = [] }: { posts: SanityDocument[] }) {
     return (
         <div>
+            <div className="h-8"></div>
             <h2 className="text-2xl text-black">Featured Posts</h2>
             <div className="h-8"></div>
             <div className="grid grid-cols-1 md:grid-cols-2  xl:grid-cols-4 gap-12">
--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import Nav from "./Nav"
 const Header = () => {
     return (
-        <header className="w-full h-24 bg-gray-100 mb-6 p-12">
+        <header className="w-full h-24 bg-gray-100 p-12">
             <div className="container mx-auto flex justify-between items-center h-full">
                 <Link href="/">
                     <h1 className="text-3xl font-bold text-gray-800">Pints of Joy</h1>
